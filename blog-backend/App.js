ed4c6a2e9b05d1b0a9c556430c63b01688c9951e
PratyushGR <pratyushrg@gmail.com>
2024-08-23 10:48:10 -0400

    updated configuration

--- blog-backend/App.js
+++ blog-backend/App.js
@@ -1,21 +1,19 @@
-const express=require('express');
+const express = require('express');
 const cors = require('cors');
 
-const app=express();
+const app = express();
 app.use(cors());
 
-const port = 3001;
+const port = process.env.PORT || 3002;
 
-app.use(express.json());    
+app.use(express.json());
 
-app.post('/submit-name',(req,res)=>
-{
-    const {name} = req.body;
+app.post('/submit-name', (req, res) => {
+    const { name } = req.body;
     console.log(`received name: ${name}`);
     res.send(`Hello ${name}!`);
 });
 
-app.listen(port,()=>{
-    console.log('Server started at http://localhost:3001');
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
 });
-
